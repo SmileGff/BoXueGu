ac0d43c4cfb87a3efe852d43284d050d2c537c63
Smile <775235052@qq.com>
2019-09-09 22:51:06 +0800

    9.9 No.2

--- app/src/main/java/boxuegu/com/boxuegu/SplashActivity.java
+++ app/src/main/java/boxuegu/com/boxuegu/SplashActivity.java
@@ -1,5 +1,6 @@
 package boxuegu.com.boxuegu;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //设置为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        init();
+    }
+    private void init(){
+
     }
 }
