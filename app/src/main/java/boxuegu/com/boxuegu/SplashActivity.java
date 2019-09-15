9894b0851d4e3dd8f35d0daf3c36d4571e0c7d8f
Smile <775235052@qq.com>
2019-09-15 12:18:50 +0800

    9.15 No.3 延迟跳转and注册界面

--- app/src/main/java/boxuegu/com/boxuegu/SplashActivity.java
+++ app/src/main/java/boxuegu/com/boxuegu/SplashActivity.java
@@ -1,21 +1,51 @@
 package boxuegu.com.boxuegu;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private TextView tv_version;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         //设置为竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
         init();
     }
     private void init(){
+        tv_version=(TextView) findViewById(R.id.tv_version);
+        try{
+            //获取包信息
+            PackageInfo info=getPackageManager().getPackageInfo(getPackageName(),0);
+            tv_version.setText("V"+info.versionName);
+        }catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
 
+        //延迟三秒跳转，timer中有一个线程，这个线程会不断执行task
+        Timer timer=new Timer();
+        //TimerTask类表示一个在指定时间内执行的task
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,
+                        SplashActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        timer.schedule(task,3000);//设置这个task在三秒后自动执行
     }
 }
